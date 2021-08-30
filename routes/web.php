fab71d8805bbeac279aef2b3e046e47ac1fd6ab0
Lucas de Souza Castellani <lucas100castellani@gmail.com>
2021-08-30 01:36:07 -0300

    Sistema de autenticação adicionado

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('site.index');
 });
+
+Route::group(['middleware' => 'auth'], function() {
+
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
