47c35960f1d30b10f3413bc156d6555a292aac4a
Lucas de Souza Castellani <lucas100castellani@gmail.com>
2021-08-30 01:27:22 -0300

    Correções de texto

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -5,9 +5,9 @@
 <div class="container-fluid header" style="background-image: url('{{ url('/img/background2.png') }}');">
     <div class="row align-items-center">
         <div class="col-md-6">
-            <h1 class="h1-copy">Quer ter um percentual de ganhos maiores na sua empresa?</h1>
+            <h1 class="h1-copy">Procura por um desenvolvedor web backend profissional? </h1>
             <hr class="fhr">
-            <h2 class="title">Conheça a GLASS Soluções em Tecnologias!</h2>
+            <h2 class="title">Conheça a GLASS Soluções em Tecnologias, fundada por Lucas S. Castellani!</h2>
         </div>
         <div class="col-md-6">
             
@@ -22,7 +22,7 @@
             </div>
             <div class="col-lg-9">
                 <h3>O QUE FAZEMOS?</h1>
-                <p class="lead">Especializados em soluções de tecnologia para qualquer tipo de negócio, a GLASS tem como objetivo solucionar qualquer tipo de necessidade que a sua empresa possua. Nosso foco é o desenvolvimento de sistemas com a melhor produtividade e facilidade para o usuário, sem abrir mão da qualidade, para que você consiga aumentar o desempenho do seu negócio.</p>
+                <p class="lead">Especializados em soluções de tecnologia para qualquer tipo de negócio, meu objetivo é prestar o melhor serviço para sua empresa como funcionário PJ, sou especializado em desenvolver soluções com boas práticas e que sejam agradáveis para seu usuário! Seja por demanda de documentações ou para criação de novos sistemas.</p>
             </div>
         </div>
     </div>
@@ -31,7 +31,7 @@
 
 <div class="container-fluid mt-3">
     <div class="row justify-content-center text-center">
-            <h2>Tecnologias mais usadas:</h2>
+            <h2>Tecnologias mais usadas por mim:</h2>
         </div>
     <div class="row text-center">
             <div class="col-md-4 my-4">
@@ -50,7 +50,7 @@
             <div class="col-md-4 my-4">
                 <i class="fas fa-palette fa-5x" style="color:orange"></i><br>
                 <h6 style="font-weight:bold;">Web Design UI/UX </h6>
-                <h6 class="mt-1">A parte que usamos para encontrar o melhor visual para seu site/sistema. É usado para deixar seu usuário confortável e para criar uma comunicação que cative o cliente, dando credibilidade, atraindo novos clientes para seu ambiente e o mais importante, deixando mais bonito!</h6>
+                <h6 class="mt-1">A parte que uso para encontrar o melhor visual para seu site/sistema. É usado para deixar seu usuário confortável e para criar uma comunicação que cative o cliente, dando credibilidade e atraindo novos clientes.</h6>
             </div>
         
     </div>
@@ -58,11 +58,11 @@
         <div class="col-md-4 my-4">
             <i class="fab fa-bootstrap fa-4x" style="color:#7a257a "></i><br>
                 <h6 style="font-weight:bold;">Bootstrap</h6>
-                <h6 class="mt-1">Um framework que facilita muito o trabalho front-end, e nos possibilita trabalhar com padrões de grid.</h6>
+                <h6 class="mt-1">Um framework que facilita muito o trabalho front-end, e nos possibilita trabalhar com padrões de grid, também trabalho com outros frameworks como Tailwind.</h6>
         </div>  
         <div class="col-md-4 my-4">
             <img src="{{ asset('/img/mysql.png') }}" style="width: 20%">
-                <h6 class="mt-1">O MySQL é simplesmente o sistema mais popular para armazenar e gerenciar dados com segurança.</h6>
+                <h6 class="mt-1">O MySQL é simplesmente o sistema mais popular para armazenar e gerenciar dados com segurança, sempre que começo uma aplicação do zero opto por utilizá-lo, mas já trabalhei com diversos outros.</h6>
         </div>  
         <div class="col-md-4 my-4">
             <i class="fab fa-digital-ocean fa-4x" style="color:blue"></i><br>
@@ -79,8 +79,8 @@
     
     <div class="row bg-dark">
         <div class="col-md-6 bordado my-auto text-light">
-            <h2 class="text-center my-2 text-light">Como podemos te ajudar?</h2>
-            <h5 id="text-center-mobile">Nós criamos sites e sistemas de maneira 100% profissional para seu negócio, indepentende do ramo. Temos alta experiência com solucionamento de quaisquer problemas que sua empresa esteja enfrentando, além de estudar e implantar novas oportunidades, criando e desenvolvendo soluções em tecnologia planejadas baseadas em anos de estudo.</h5>
+            <h2 class="text-center my-2 text-light">Como posso te ajudar?</h2>
+            <h5 id="text-center-mobile">Eu crio sites e sistemas de maneira 100% profissional para seu negócio, indepentende do ramo. Tenho alta experiência com solucionamento de quaisquer problemas que sua empresa esteja enfrentando, além de estudar e implantar novas funcionalidades, criando e desenvolvendo soluções em tecnologia planejadas baseada em documentações ou sprints.</h5>
             <br>
             <h5 class="text-center">Veja alguns exemplos de tecnologias desenvolvidas:</h5>
         </div>
@@ -112,24 +112,24 @@
     <hr>
     <div class="row justify-content-center">
         <br>
-        <h4 class="text-center pilares-title">Saiba quais são os 8 pilares que usamos para um briefing assertivo e um plano de execução para desenvolver um projeto de valor e <u class="text-primary">RESULTADOS:</u></h4>
+        <h4 class="text-center pilares-title">Saiba quais são os 8 pilares que uso para um briefing assertivo e um plano de execução para desenvolver um projeto de valor e <u class="text-primary">RESULTADOS:</u></h4>
         <div class="col-md-4">
             <h5 class="pilares-subtitle"><i class="fas fa-check-circle text-primary"></i>&nbspDetalhes do projeto;</h5>
-            <p id="m-a">Entendemos totos os aspectos do seu projeto para um desenvolvimento otimizado.</p>
+            <p id="m-a">Procuro entender todos os aspectos do seu projeto para um desenvolvimento otimizado.</p>
             <h5 class="pilares-subtitle"><i class="fas fa-check-circle text-primary"></i>&nbspDomínios registrados;</h5>
-            <p id="m-a">Recomendamos sempre mais de um domínio e nomes que tenham a ver com o nicho: .com e .br</p>
+            <p id="m-a">Recomendo sempre mais de um domínio e nomes que tenham a ver com o nicho: .com e .br (caso seja um novo sistema)</p>
             <h5 class="pilares-subtitle"><i class="fas fa-check-circle text-primary"></i>&nbspPlanejamento de tráfego;</h5>
             <p id="m-a">Devemos ter em mente que precisamos desenvolver meios de tráfego para que seu site tenha acesso, podendo ser por meio de SEO, redes sociais ou tráfego pago.</p>
             <h5 class="pilares-subtitle"><i class="fas fa-check-circle text-primary"></i>&nbsp5 principais problemas para resolver;</h5>
-            <p id="m-a">Analisamos suas maiores necessidades, com nossa experiência a maioria deles são quase sempre atrair mais clientes, melhorar o atendimento, investir e obter resultado, vender mais, ficar a frente dos concorrentes.</p>
+            <p id="m-a">Analiso suas maiores necessidades, com nossa experiência a maioria deles são quase sempre atrair mais clientes, melhorar o atendimento, investir e obter resultado, vender mais, ficar a frente dos concorrentes.</p>
         </div>
         <div class="col-md-4">
             <h5 class="pilares-subtitle"><i class="fas fa-check-circle text-primary"></i>&nbsp3 principais concorrentes;</h5>
-            <p id="m-a">Analisamos os concorrentes do seu nicho, para o desenvolvimento de um projeto que seja único e não saia atrás no mercado.</p>
+            <p id="m-a">Procuro os principais concorrentes do seu nicho, para o desenvolvimento de um projeto que seja único e não saia atrás no mercado.</p>
             <h5 class="pilares-subtitle"><i class="fas fa-check-circle text-primary"></i>&nbspReferência de mercado;</h5>
-            <p id="m-a">Criamos uma estratégia de briefing para que desenvolvamos um projeto não só que solucione seus problemas, mas que vire referência no mercado.</p>
+            <p id="m-a">Desenvolvo uma estratégia de briefing para que desenvolvamos um projeto não só que solucione seus problemas, mas que vire referência no mercado.</p>
             <h5 class="pilares-subtitle"><i class="fas fa-check-circle text-primary"></i>&nbspLista de email marketing;</h5>
-            <p id="m-a">Analisamos que frequência seria ideal para sua lista de emails, e que ferramentas usar.</p>
+            <p id="m-a">Analiso que frequência seria ideal para sua lista de emails, e que ferramentas usar, tendo como referência os concorrentes.</p>
             <h5 class="pilares-subtitle"><i class="fas fa-check-circle text-primary"></i>&nbspLogo e padrão de cores;</h5>
             <p id="m-a">E por fim, mas não menos importante, a identidade visual do projeto baseado em psicologia das cores e formas que tenham conexão com o nicho.</p>
         </div>
@@ -147,7 +147,7 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Daniela Martins</h5>
-                    <p>"Precisava de um site de roupas para expandir o meu negócio e a GLASS me ajudou muito criando um site com sistemas excepcionais integrados, já fechei mais de 2 negócios com eles e estou ansiosa para ver até aonde vai essa parceria"</p>
+                    <p>"Precisava de um site de roupas para expandir o meu negócio e o Lucas me ajudou muito criando um site com sistemas excepcionais integrados, já fechei mais de 2 negócios com ele"</p>
                 </div>
             </div>
         </div>
@@ -155,7 +155,7 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Leonardo Castellani</h5>
-                    <p>"Sou Youtuber e precisava de um sistema para otimização dos meus roteiros, e a GLASS me ajudou muito nisso, criando um programa em que eu consigo definir toda trama dos meus textos de forma automatizada"</p>
+                    <p>"Sou Youtuber e precisava de um sistema para otimização dos meus roteiros, e o Lucas me ajudou muito nisso, criando um programa em que eu consigo definir toda trama dos meus textos de forma automatizada"</p>
                 </div>
             </div>
         </div>
@@ -163,7 +163,7 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">João Victor</h5>
-                    <p>"Contratei a GLASS no intuito de que assumissem parte da minha equipe de TI, atualmente sou CTO de uma empresa de desenvolvimento e fiquei impressionado com o que foi entregue, muito mais do que o esperado, recomendo"</p>
+                    <p>"Contratei o Lucas no intuito de que assumissem um cargo da minha equipe de TI, atualmente sou CTO de uma empresa de desenvolvimento e fiquei impressionado com o que foi entregue, muito mais do que o esperado, recomendo"</p>
                 </div>
             </div>
         </div>
@@ -173,7 +173,7 @@
 <div class="container text-center justify-content-center my-4">
         <h3>Vamos encontrar juntos a solução para seu negócio!</h3>
         <i class="fas fa-hand-point-down text-primary fa-3x"></i><br/>
-        <a href="https://api.whatsapp.com/send?phone=5511978512654"><button class="my-2 btn btn-primary contatBtnIndex">QUERO ENTRAR EM CONTATO!</button></a>
+        <a href="https://api.whatsapp.com/send?phone=5511978512654"><button class="my-2 btn btn-primary contatBtnIndex">QUERO SABER MAIS!</button></a>
 </div>
 
 <div class="container-fluid bg-dark">
@@ -183,7 +183,7 @@
                 <a href=""><i class="fas fa-map-marker-alt fa-3x my-3 text-light"></i></a>
             </div>
             <div class="col-lg-4">
-                <a href="mailto:admin@glasstecnologias.com"><i class="fas fa-envelope fa-3x my-3 text-light"></i></a>
+                <a href="mailto:lucasdesouzacastellani@gmail.com"><i class="fas fa-envelope fa-3x my-3 text-light"></i></a>
             </div>
             <div class="col-lg-4">
                 <a href="tel:+55 (11) 97851-2654"><i class="fas fa-phone-alt fa-3x my-3 text-light"></i></a>
@@ -194,7 +194,7 @@
                 São Paulo - Brasil
             </div>
             <div class="col-lg-4 mb-3">
-                admin@glasstecnologias.com
+                lucasdesouzacastellani@gmail.com
             </div>
             <div class="col-lg-4 mb-3">
                 +55 (11) 97851-2654
@@ -203,9 +203,5 @@
     </div>
 </div>
 <div class="container-fluid text-center">2021 &copy Copyright - Todos os direitos reservados. Desenvolvido por GLASS</div>
-
-
-
-
 
 @endsection
